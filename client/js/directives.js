f3cab2989b965e5305e21b2a8cb5d8925116e6f6
Toks Fifo <toksfifo@gmail.com>
2014-11-30 02:54:16 -0500

    styling home page

--- client/js/directives.js
+++ client/js/directives.js
@@ -6,8 +6,22 @@ count_app.directive('card', function() {
 		// },
 		templateUrl: 'client/views/card.html',
 		// transclude: true,
-		controller: function($scope) {
-			// console.log('card directive in action');
-		}
+		// controller: function($scope) {
+		// 	console.log('card directive in action');
+		// }
 	};
 });
+
+count_app.directive('scrollToItem', function() {                                         
+    return {                                                                                 
+        restrict: 'A',                                                                       
+        scope: {                                                                             
+            scrollTo: "@"                                                                    
+        },                                                                                   
+        link: function(scope, $elm, attr) {                                                   
+            $elm.on('click', function() {                                                    
+                $('html,body').animate({scrollTop: $(scope.scrollTo).offset().top }, 1000);
+            });                                                                              
+        }                                                                                    
+    };
+});     
